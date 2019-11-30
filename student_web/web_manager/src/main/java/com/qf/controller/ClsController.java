6934f3fcefbbd78d4977282d2259ae4661f25888
chen <chen@qq.com>
2019-11-30 16:30:24 +0800

    修改了一下web-Manager下面的依赖,让它依赖于父项目 Manager的依赖

--- student_web/web_manager/src/main/java/com/qf/controller/ClsController.java
+++ student_web/web_manager/src/main/java/com/qf/controller/ClsController.java
@@ -1,7 +1,9 @@
 package com.qf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("cls")
 public class ClsController {
 }
