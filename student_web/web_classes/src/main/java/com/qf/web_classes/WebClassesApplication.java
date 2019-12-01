e0fdd50a0b7421fab3c9e74f3d36f943bb9ea024
chen <chen@qq.com>
2019-12-01 17:21:36 +0800

    修改1.0

--- student_web/web_classes/src/main/java/com/qf/web_classes/WebClassesApplication.java
+++ student_web/web_classes/src/main/java/com/qf/web_classes/WebClassesApplication.java
@@ -1,12 +1,13 @@
 package com.qf.web_classes;
 
 import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.qf")
 @DubboComponentScan("com.qf.web_classes.serviceimpl")
-
+@MapperScan("com.qf.web_classes.dao")
 public class WebClassesApplication {
 
     public static void main(String[] args) {
